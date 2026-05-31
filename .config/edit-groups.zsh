37d2ef676b4828dcac75f29fe1cff3858177bc2b
CrowApples <thewizardking@gmail.com>
2026-05-31 10:12:06 -0400

    added misc .config files

---
+++ .config/edit-groups.zsh
@@ -0,0 +1,26 @@
+hconfig_list=(
+	/home/alex/.config/hypr/hyprland/workspaces.lua
+
+	/home/alex/.config/hypr/hyprland.lua
+	/home/alex/.config/hypr/hyprland/general.lua
+	
+	/home/alex/.config/hypr/hyprland/window_rules.lua
+	/home/alex/.config/hypr/hyprland/keybindings.lua
+	/home/alex/.config/hypr/hyprland/autostart.lua
+	/home/alex/.config/hypr/.less-accessed-hypr-config-files
+
+)
+
+
+#consider creating a sublist or symlink folder for less used config files and auto-open that as the last file here.
+
+wbconfig_list=(
+	/home/alex/.config/waybar/config
+	/home/alex/.config/waybar/style.css
+	/home/alex/.config/waybar/modules.json
+)
+
+nconfig_list=(
+	/home/alex/.config/nvim/lua/plugins/init.lua
+	/home/alex/.config/nvim/init.lua
+	)
